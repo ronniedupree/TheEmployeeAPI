a0cd87ba558221a11c3914b770c420dbfd0bc8d9
Ronald DuPree Jr. <rdupr23@wgu.edu>
2025-12-30 05:24:58 -0800

    initial commit

---
+++ TheEmployeeApi/Program.cs
@@ -0,0 +1,48 @@
+using TheEmployeeAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+var employees = new List<Employee>
+{
+    new Employee { Id = 1, FirstName = "John", LastName = "Doe" },
+    new Employee { Id = 2, FirstName = "Jane", LastName = "Doe" }
+};
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+var employeeRoute = app.MapGroup("employees");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+employeeRoute.MapGet(string.Empty, () =>
+{
+    return Results.Ok(employees);
+});
+
+employeeRoute.MapGet("{id:int}", (int id) =>
+{
+    var employee = employees.SingleOrDefault(e => e.Id == id);
+    if (employee == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(employee);
+});
+
+employeeRoute.MapPost(string.Empty, (Employee employee) =>
+{
+    employee.Id = employees.Max(e => e.Id) + 1;
+    employees.Add(employee);
+    return Results.Created($"/employees/{employee.Id}", employee);
+});
+
+app.Run();
