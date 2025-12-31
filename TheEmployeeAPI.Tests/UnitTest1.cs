dfcf9416a221abd005402e70f3abeec7d4ab1d1f
Ronald DuPree Jr. <rdupr23@wgu.edu>
2025-12-31 01:32:12 -0800

    tests

---
+++ TheEmployeeAPI.Tests/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TheEmployeeAPI.Employees;
+
+namespace TheEmployeeAPI.Tests;
+
+public class BasicTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+{
+    [Fact]
+    public async Task GetAllEmployees_ReturnsOkResult()
+    {
+        var client = factory.CreateClient();
+        var response = await client.GetAsync("/employees");
+        
+        response.EnsureSuccessStatusCode();
+    }
+    
+    
+    [Fact]
+    public async Task GetEmployeeById_ReturnsOkResult()
+    {
+        var client = factory.CreateClient();
+        var response = await client.GetAsync("/employees/1");
+
+        response.EnsureSuccessStatusCode();
+    }
+
+    
+    [Fact]
+    public async Task CreateEmployee_ReturnsCreatedResult()
+    {
+        var client = factory.CreateClient();
+        var response = await client.PostAsJsonAsync("/employees", new Employee { FirstName = "John", LastName = "Doe", SocialSecurityNumber = "123-45-6789" });
+
+        response.EnsureSuccessStatusCode();
+    }
+
+    
+    [Fact]
+    public async Task CreateEmployee_ReturnsBadRequestResult()
+    {
+        // Arrange
+        var client = factory.CreateClient();
+        var invalidEmployee = new CreateEmployeeRequest(); // Empty object to trigger validation errors
+
+        // Act
+        var response = await client.PostAsJsonAsync("/employees", invalidEmployee);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+        var problemDetails = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+        Assert.NotNull(problemDetails);
+        Assert.Contains("FirstName", problemDetails.Errors.Keys);
+        Assert.Contains("LastName", problemDetails.Errors.Keys);
+        Assert.Contains("'First Name' must not be empty.", problemDetails.Errors["FirstName"]);
+        Assert.Contains("'Last Name' must not be empty.", problemDetails.Errors["LastName"]);
+    }
+    
+    
+    [Fact]
+    public async Task UpdateEmployee_ReturnsOkResult()
+    {
+        var client = factory.CreateClient();
+        var response = await client.PutAsJsonAsync("/employees/1", new Employee { FirstName = "John", LastName = "Doe", SocialSecurityNumber = "123-45-6789"});
+
+        response.EnsureSuccessStatusCode();
+    }
+    
+    
+    [Fact]
+    public async Task UpdateEmployee_ReturnsNotFoundForNonExistingEmployee()
+    {
+        var client = factory.CreateClient();
+        var response = await client.PutAsJsonAsync("/employees/99999", new Employee { FirstName = "John", LastName = "Doe", SocialSecurityNumber = "123-45-6789" });
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
